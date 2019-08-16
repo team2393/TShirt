ab96da53b7a3a23503b779c37351c53a45ef1cb8
Kay <kay.kasemir@gmail.com>
2019-08-16 18:35:20 -0400

    Use array for tubes

--- src/main/java/frc/robot/TShirtRobot.java
+++ src/main/java/frc/robot/TShirtRobot.java
@@ -13,22 +13,25 @@ public class TShirtRobot extends TimedRobot
 {
     //Define Tubes
     private final DigitalOutput tube_a = new DigitalOutput(RobotMap.TUBE_A);
-    private Command fire_a = new FireCommand(tube_a);
     private final DigitalOutput tube_b = new DigitalOutput(RobotMap.TUBE_A);
-    private Command fire_b = new FireCommand(tube_b);
     private final DigitalOutput tube_c = new DigitalOutput(RobotMap.TUBE_A);
-    private Command fire_c = new FireCommand(tube_c);
+    private Command[] fire_commands = new Command[]
+    {
+        new FireCommand(tube_a),
+        new FireCommand(tube_b),
+        new FireCommand(tube_c)
+    };
 
-    private Command fire = fire_a;
-
+    private int active_tube = 0;
+    
     /** Run once on startup */
     @Override
     public void robotInit()
     {
         // Put some commands onto dashboard
-        SmartDashboard.putData("Fire A", fire_a);
-        SmartDashboard.putData("Fire B", fire_b);
-        SmartDashboard.putData("Fire C", fire_c);
+        SmartDashboard.putData("Fire A", fire_commands[0]);
+        SmartDashboard.putData("Fire B", fire_commands[1]);
+        SmartDashboard.putData("Fire C", fire_commands[2]);
     }
 
     /** Called all the time */
@@ -43,20 +46,14 @@ public class TShirtRobot extends TimedRobot
     @Override
     public void teleopPeriodic()
     {
-        // TODO Is there just one tube solenoid for all tubes?
         // Is there a way to select a tube?
         // Or should presssing 'fire' always fire all tubes in some sequence?
         if (OI.isFirePressed())
         {
             // Fire the active command
-            fire.start();
+            fire_commands[active_tube].start();
             // Prepare to cycle to next tube
-            if (fire == fire_a)
-                fire = fire_b;
-            else if (fire == fire_b)
-                fire = fire_c;
-            else
-                fire = fire_a;
+            active_tube = (active_tube + 1) % 3;
         }
     }
 }
