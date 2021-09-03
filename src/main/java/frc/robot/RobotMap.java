59556204739e9e1cfa18e1aab887042559f662a7
Kay <kay.kasemir@gmail.com>
2021-09-03 18:58:03 -0400

    test

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -5,7 +5,7 @@ package frc.robot;
  */
 public class RobotMap
 {
-    // TODO What _is_ connected to the RoboRIO?
+    // TODO Determine what is connected to the RoboRIO
     
     // Drive motors on CAN bus
     public static final int LEFT_MASTER = 1;
