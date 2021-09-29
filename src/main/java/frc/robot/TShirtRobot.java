9291708fa0edeac6e56b9ce2cdbcfd2dd845f6e3
fruitypig <fruitypigy@gmail.com>
2021-09-29 16:37:51 -0400

    fixed the tube fireing

--- src/main/java/frc/robot/TShirtRobot.java
+++ src/main/java/frc/robot/TShirtRobot.java
@@ -18,14 +18,22 @@ public class TShirtRobot extends TimedRobot
 
     private final VictorSP left_motor = new VictorSP(RobotMap.PWM_LEFT);
     private final VictorSP right_motor = new VictorSP(RobotMap.PWM_RIGHT);
-    DifferentialDrive drive = new DifferentialDrive(left_motor, right_motor);
+    private DifferentialDrive drive = new DifferentialDrive(left_motor, right_motor);
     
     // Pressure sensors
-    DigitalInput top_sensor = new DigitalInput(RobotMap.DIO_TOP_SENSOR);
-    DigitalInput left_sensor = new DigitalInput(RobotMap.DIO_LEFT_SENSOR);
-    DigitalInput right_sensor = new DigitalInput(RobotMap.DIO_RIGHT_SENSOR);
+    private DigitalInput top_sensor = new DigitalInput(RobotMap.DIO_TOP_SENSOR);
+    private DigitalInput left_sensor = new DigitalInput(RobotMap.DIO_LEFT_SENSOR);
+    private DigitalInput right_sensor = new DigitalInput(RobotMap.DIO_RIGHT_SENSOR);
 
-    Solenoid top = new Solenoid(RobotMap.TOP_SOLENOID);
+    private Solenoid top = new Solenoid(RobotMap.TOP_SOLENOID);
+    private Solenoid left = new Solenoid(RobotMap.LEFT_SOLENOID);
+    private Solenoid right = new Solenoid(RobotMap.RIGHT_SOLENOID);
+
+    private long fire_top_time = 0;
+    private long fire_left_time = 0;
+    private long fire_right_time = 0;
+
+    private long remain_off = 0;
 
     /** Run once on startup */
     @Override
@@ -65,8 +73,38 @@ public class TShirtRobot extends TimedRobot
         SmartDashboard.putBoolean("Right Sensor", right_sensor.get());
         SmartDashboard.putBoolean("Left Sensor", left_sensor.get());
 
-        top.set(OI.isTopPressed());
-        // left_tube.set(OI.isLeftPressed());
-        // right_tube.set(OI.isRightPressed());
+        left.set(OI.isLeftPressed());
+        right.set(OI.isRightPressed());
+
+        final long now = System.currentTimeMillis();
+
+        // Is the button pressed, and it's OK to fire again?
+        if (OI.isTopPressed()  &&  now > remain_off)
+        {
+            // Fire top tube from now until ... ms
+            fire_top_time = now + 500;
+            // Then remain off until 2 seconds from now
+            remain_off = now + 2000;
+        }
+    
+        if (OI.isLeftPressed()  &&  now > remain_off)
+        {
+            // Fire top tube from now until ... ms
+            fire_left_time = now + 500;
+            // Then remain off until 2 seconds from now
+            remain_off = now + 2000;
+        }
+        
+        if (OI.isRightPressed()  &&  now > remain_off)
+        {
+            // Fire top tube from now until ... ms
+            fire_right_time = now + 500;
+            // Then remain off until 2 seconds from now
+            remain_off = now + 2000;
+        }
+        
+        top.set(now < fire_top_time);
+        left.set(now < fire_left_time);
+        right.set(now < fire_right_time);
     }
 }
